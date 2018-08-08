483ef2ada1bb930ed30caa9a745a380e23447042
Curtis Cawley <curtis.cawley@icloud.com>
2018-08-08 13:43:38 -0700

    added user_type column based on feedback

--- db/migrations/20180807162355_users.js
+++ db/migrations/20180807162355_users.js
@@ -3,6 +3,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', table => {
     table.increments('id');
+    table.string('user_type');
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
     table.string('user_name').notNullable();
