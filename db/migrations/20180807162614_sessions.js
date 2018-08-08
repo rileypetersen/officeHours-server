a5d59347d3c505c77ef615f3b4a52ccd3ba6a37e
Curtis Cawley <curtis.cawley@icloud.com>
2018-08-07 18:39:33 -0700

    filled out sessions table with column info

--- db/migrations/20180807162614_sessions.js
+++ db/migrations/20180807162614_sessions.js
@@ -1,8 +1,19 @@
+// 'sessions' table
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('sessions', table => {
+    table.increments.id('id');
+    table.integer('user_id').notNullable();
+    table.foreign('user_id').references('users.id').onDelete('CASCADE');
+    table.integer('organization_id').notNullable();
+    table.foreign('organization_id').references('organizations.id').onDelete('CASCADE');
+    table.string('date').notNullable();
+    table.string('location').notNullable();
+    table.string('duration').notNullable();
+    table.string('delay').notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+
 };
