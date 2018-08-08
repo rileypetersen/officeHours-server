1d4cfefcda0adad79cf6b763a03f6e613bfcc27d
Curtis Cawley <curtis.cawley@icloud.com>
2018-08-08 13:55:37 -0700

    made some changes to column names and consistency in naming

--- db/migrations/20180807162629_meetings.js
+++ db/migrations/20180807162629_meetings.js
@@ -2,15 +2,15 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('meetings', table => {
-    table.increments.id('id');
+    table.increments.id('meeting_id');
     table.integer('member_id').notNullable();
-    table.foreign('member_id').references('users.id').onDelete('CASCADE');
+    table.foreign('member_id').references('users.user_id').onDelete('CASCADE');
     table.integer('host_id').notNullable();
-    table.foreign('host_id').references('users.id').onDelete('CASCADE');
+    table.foreign('host_id').references('users.user_id').onDelete('CASCADE');
     table.integer('organization_id').notNullable();
-    table.foreign('organization_id').references('organizations.id').onDelete('CASCADE');
+    table.foreign('organization_id').references('organizations.organization_id').onDelete('CASCADE');
     table.integer('session_id').notNullable();
-    table.foreign('session_id').references('sessions.id').onDelete('CASCADE');
+    table.foreign('session_id').references('sessions.session_id').onDelete('CASCADE');
     table.text('location').notNullable();
     table.time('duration').notNullable();
     table.time('delay').notNullable();
--- db/migrations/20180807162635_tags.js
+++ db/migrations/20180807162635_tags.js
@@ -2,11 +2,11 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('tags', table => {
-    table.increments.id('id');
+    table.increments.id('tag_id');
     table.integer('user_id');
-    table.foreign('user_id').references('users.id').onDelete('CASCADE');
+    table.foreign('user_id').references('users.user_id').onDelete('CASCADE');
     table.integer('organization_id');
-    table.foreign('organization_id').references('organizations.id').onDelete('CASCADE');
+    table.foreign('organization_id').references('organizations.organization_id').onDelete('CASCADE');
     table.string('name').notNullable();
   });
 };
