2e34335bd43ae7a703cab3bcb49c8d4dd54c216f
Curtis Cawley <curtis.cawley@icloud.com>
2018-08-07 19:32:56 -0700

    filled out with column info

--- db/migrations/20180807162635_tags.js
+++ db/migrations/20180807162635_tags.js
@@ -1,8 +1,16 @@
+// 'tags' table
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('tags', table => {
+    table.increments.id('id');
+    table.integer('user_id');
+    table.foreign('user_id').references('users.id').onDelete('CASCADE');
+    table.integer('organization_id');
+    table.foreign('organization_id').references('organizations.id').onDelete('CASCADE');
+    table.string('name').notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('tags');
 };
