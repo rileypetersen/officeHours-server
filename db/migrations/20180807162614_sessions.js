9bd2ec2eaf8cd6c93dffcb26d9ea9e565db80285
Curtis Cawley <curtis.cawley@icloud.com>
2018-08-07 19:49:00 -0700

    updated schema building keywords to be more appropriate in certain cases

--- db/migrations/20180807162614_sessions.js
+++ db/migrations/20180807162614_sessions.js
@@ -7,10 +7,9 @@ exports.up = function(knex, Promise) {
     table.foreign('user_id').references('users.id').onDelete('CASCADE');
     table.integer('organization_id').notNullable();
     table.foreign('organization_id').references('organizations.id').onDelete('CASCADE');
-    table.string('date').notNullable();
-    table.string('location').notNullable();
-    table.string('duration').notNullable();
-    table.string('delay').notNullable();
+    table.date('date').notNullable();
+    table.text('location').notNullable();
+    table.time('delay').notNullable();
   });
 };
 
