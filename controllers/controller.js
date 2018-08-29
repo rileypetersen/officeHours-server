4525157843606ade2cd956522d7e9d92a62ad785
Justin <justhays124@gmail.com>
2018-08-29 15:57:51 -0700

    generic OOP CRUD controller

---
+++ controllers/controller.js
@@ -0,0 +1,34 @@
+module.exports = name => {
+    const Model = require(`../model/${ name }.controllers.js`)
+
+    class Controller {
+        
+        static all(req, res, next) {
+            Model.all()
+                .then(response => res.status(201).json({ [name]: response }))
+        }
+
+        static one(req, res, next) {
+            Model.one(req.params.id)
+                .then(response => res.status(201).json({ [name]: response }))
+        }
+
+        static create(req, res, next) {
+            Model.create(req.body.create)
+                .then(response => res.status(201).json({ [name]: response }))
+        }
+
+        static update(req, res, next) {
+            Model.update(req.params.id, req.body)
+                .then(response => res.status(200).json({ [name]: response }))
+        }
+
+        static destroy(req, res, next) {
+            Model.destroy(req.params.id)
+                .then(response => res.status(202).json({ [name]: response }))
+        }
+
+    }
+
+    return Controller
+};
