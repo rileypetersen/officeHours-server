ae8c4711dfb5b08dad3b7150828241f882ae067b
Justin <justhays124@gmail.com>
2018-08-30 09:50:53 -0700

    users.controller validation

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -9,19 +9,24 @@ class Users extends Controller {
   } 
 
   static isValidUserCreate(req, res, next) {
-    const { name, username, email, password, zipcode, phone } = req.body
-    if (!name || typeof name !== 'string') throw new Error ('registerNameWrong')
-    if (!username || typeof username !== 'string') throw new Error ('registerUsernameWrong')
-    if (!password || typeof password !== 'string') throw new Error ('registerPasswordWrong')
-    if (!zipcode || typeof zipcode !== 'number') throw new Error ('registerZipWrong')
-    if (!phone || typeof phone !== 'string') throw new Error ('registerPhoneWrong')
-    if (!email || typeof email !== 'string') throw new Error ('registerEmailWrong')
+    const { user_type, first_name, last_name, user_name, profile_img_url, title, short_description, long_description, linkedin_url, website_url, can_create_session } = req.body
+    if (!user_type || typeof user_type !== 'string') throw new Error('badUserType')
+    if (!first_name || typeof first_name !== 'string') throw new Error('badFirstName')
+    if (!last_name || typeof last_name !== 'string') throw new Error('badLastName')
+    if (!user_name || typeof user_name !== 'string') throw new Error('badUserName')
+    if (!profile_img_url || typeof profile_img_url !== 'string') throw new Error('badProfileImg')
+    if (!title || typeof title !== 'string') throw new Error('badTitle')
+    if (!short_description || typeof short_description !== 'string') throw new Error('badShortDescription')
+    if (!long_description || typeof long_description !== 'string') throw new Error('badLongDescription')
+    if (!linkedin_url || typeof linkedin_url !== 'string') throw new Error('badLinkedinURL')
+    if (!website_url || typeof website_url !== 'string') throw new Error('badWebsiteURL')
+    if (typeof can_create_session !== 'boolean') throw new Error('badCanCreateSession')
     next();
   }
 
   static isValidUserPatch(req, res, next) {
-    const { name, username, email, password, zipcode, phone } = req.body
-    if (!name && !username && !email && !password && !zipcode && !phone) throw new Error('aFieldRequired')
+    const { user_type, first_name, last_name, user_name, profile_img_url, title, short_description, long_description, linkedin_url, website_url, can_create_session } = req.body
+    if (!user_type && !first_name && !last_name && !user_name && !profile_img_url && !title && !short_description && !long_description && !linkedin_url && !website_url) throw new Error('aFieldRequired')
     next();
   }
 
