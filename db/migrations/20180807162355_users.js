7a8cd893a1be669f34bf3dd74f960a43987b61ef
Justin <justhays124@gmail.com>
2018-08-30 10:17:51 -0700

    users table updated

--- db/migrations/20180807162355_users.js
+++ db/migrations/20180807162355_users.js
@@ -7,6 +7,7 @@ exports.up = function(knex, Promise) {
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
     table.string('user_name').notNullable();
+    table.string('password').notNullable();
     table.text('profile_img_url').notNullable().defaultTo('');
     table.string('title').notNullable();
     table.string('short_description').notNullable();
