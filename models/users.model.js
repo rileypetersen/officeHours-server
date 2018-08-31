2018a705dc760edb4a2023bfeb02d4cbdf8d01e4
Justin <justhays124@gmail.com>
2018-08-31 09:32:33 -0700

    added unique user create method

--- models/users.model.js
+++ models/users.model.js
@@ -1,11 +1,21 @@
 const Model = require('./model.js')(`users`);
 const knex = require('../db/knex');
+const bcrypt = require('bcryptjs')
 
 
 class UsersModel extends Model {
   constructor(){
     super()
   }
+
+  static create(body) {
+    body.hashed_password = bcrypt.hashSync(body.password)
+    delete body.password
+    return knex('users')
+        .insert(body)
+        .returning('*')
+        .then((res) => res)
+  };
 
   static getUserByUsername(user_name) {
     return knex('users')
