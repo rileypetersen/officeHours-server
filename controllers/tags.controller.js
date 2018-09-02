64fb2f501656330c38d59a1e99c79ad8d778c8f1
Justin <justhays124@gmail.com>
2018-09-02 15:33:08 -0700

    tags routes prep

---
+++ controllers/tags.controller.js
@@ -0,0 +1,26 @@
+const Controller = require('./Controller.js')(`Tags`);
+const { UsersModel, OrganizationsModel } = require('../models');
+const validate = require('../middleware/validations');
+
+
+class TagsController extends Controller {
+  constructor(){
+    super()
+  };
+
+  static isValidTagCreate(req, res, next) {
+    // validate.tagCreate(req.body)
+      
+    //   .catch(err => next(err));
+  };
+
+  static isValidTagPatch(req, res, next) {
+    // validate.orgUpdate(req.body)
+      
+    //   .catch(err => next(err));
+  };
+
+};
+
+
+module.exports = TagsController;
--- models/tags.model.js
+++ models/tags.model.js
@@ -1,1 +1,13 @@
+const Model = require('./model.js')(`tags`);
 const knex = require('../db/knex');
+
+
+class TagsModel extends Model {
+  constructor(){
+    super()
+  };
+
+};
+
+
+module.exports= TagsModel
