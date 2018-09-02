a1a682597cd5319cce7540f499cfaae9568a8d33
Justin <justhays124@gmail.com>
2018-09-02 15:05:07 -0700

    implemented moment in seed data and converted select types

--- db/migrations/20180807162614_sessions.js
+++ db/migrations/20180807162614_sessions.js
@@ -7,11 +7,11 @@ exports.up = function(knex, Promise) {
     table.foreign('user_id').references('users.id').onDelete('CASCADE');
     table.integer('organization_id').notNullable();
     table.foreign('organization_id').references('organizations.id').onDelete('CASCADE');
-    table.date('date').notNullable();
-    table.time('start_time').notNullable();
+    table.string('date').notNullable();
+    table.string('start_time').notNullable();
     table.text('location').notNullable();
-    table.time('duration').notNullable();
-    table.time('delay').notNullable();
+    table.string('duration').notNullable();
+    table.string('delay').notNullable();
   });
 };
 
--- db/seeds/03_sessions.js
+++ db/seeds/03_sessions.js
@@ -1,3 +1,7 @@
+const moment = require('moment')
+const dateFormat = 'MM-DD-YYYY'
+const timeFormat = '+-HH:mm a'
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('sessions').del()
@@ -8,8 +12,8 @@ exports.seed = function(knex, Promise) {
           id: 1,
           user_id: 4,
           organization_id: 1,
-          date: '10/11/2018',
-          start_time: '7:00am',
+          date: moment('10/11/2018', dateFormat),
+          start_time: moment('7:00am', timeFormat),
           location: 'Starbucks Reserve',
           duration: '2:00',
           delay: '0:15'
@@ -18,8 +22,8 @@ exports.seed = function(knex, Promise) {
           id: 2,
           user_id: 3,
           organization_id: 2,
-          date: '10/15/2018',
-          start_time: '11:00am',
+          date: moment('10/15/2018', dateFormat),
+          start_time: moment('11:00am', timeFormat),
           location: 'Comotion Labs',
           duration: '4:00',
           delay: '0:30'
@@ -28,8 +32,8 @@ exports.seed = function(knex, Promise) {
           id: 3,
           user_id: 3,
           organization_id: 2,
-          date: '10/16/2018',
-          start_time: '1:00pm',
+          date: moment('10/16/2018', dateFormat),
+          start_time: moment('1:00pm', timeFormat),
           location: 'WeWork Holyoke',
           duration: '6:00',
           delay: '1:30'
@@ -38,8 +42,8 @@ exports.seed = function(knex, Promise) {
           id: 4,
           user_id: 3,
           organization_id: 2,
-          date: '10/17/2018',
-          start_time: '2:00pm',
+          date: moment('10/17/2018', dateFormat),
+          start_time: moment('2:00pm', timeFormat),
           location: 'Caffe Umbria',
           duration: '3:00',
           delay: '0:45'
@@ -48,8 +52,8 @@ exports.seed = function(knex, Promise) {
           id: 5,
           user_id: 5,
           organization_id: 1,
-          date: '10/18/2018',
-          start_time: '11:00am',
+          date: moment('10/18/2018', dateFormat),
+          start_time: moment('11:00am', timeFormat),
           location: 'Starbucks Reserve',
           duration: '2:00',
           delay: '0:15'
@@ -58,8 +62,8 @@ exports.seed = function(knex, Promise) {
           id: 6,
           user_id: 5,
           organization_id: 1,
-          date: '10/23/2018',
-          start_time: '1:00pm',
+          date: moment('10/23/2018', dateFormat),
+          start_time: moment('1:00pm', timeFormat),
           location: 'Starbucks Reserve',
           duration: '4:00',
           delay: '0:30'
@@ -68,8 +72,8 @@ exports.seed = function(knex, Promise) {
           id: 7,
           user_id: 4,
           organization_id: 1,
-          date: '10/25/2018',
-          start_time: '2:00pm',
+          date: moment('10/25/2018', dateFormat),
+          start_time: moment('2:00pm', timeFormat),
           location: 'Starbucks Reserve',
           duration: '6:00',
           delay: '1:00'
@@ -78,8 +82,8 @@ exports.seed = function(knex, Promise) {
           id: 8,
           user_id: 4,
           organization_id: 1,
-          date: '10/24/2018',
-          start_time: '11:00am',
+          date: moment('10/24/2018', dateFormat),
+          start_time: moment('11:00am', timeFormat),
           location: 'Starbucks Reserve',
           duration: '3:00',
           delay: '0:30'
@@ -88,8 +92,8 @@ exports.seed = function(knex, Promise) {
           id: 9,
           user_id: 5,
           organization_id: 1,
-          date: '10/31/2018',
-          start_time: '1:00pm',
+          date: moment('10/31/2018', dateFormat),
+          start_time: moment('1:00pm', timeFormat),
           location: 'Starbucks Reserve',
           duration: '2:00',
           delay: '0:15'
