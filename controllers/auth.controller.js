f77b1aab403d317a8dd47b7fc26e5275878cbf67
Justin <justhays124@gmail.com>
2018-09-06 16:43:48 -0700

    setup for auth controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,25 @@
+const authModel = require('../../models/Shop/auth')
+const jwt = require('jsonwebtoken')
+
+
+class Auth {
+	
+	static isAuthorized(req, res, next) {
+		if (!req.headers.authorization) throw new Error('userUnauthorized')
+		const [scheme, credentials] = req.headers.authorization.split(' ')
+		jwt.verify(credentials, process.env.SECRET, (err, payload) => {
+			if (err) throw new Error('userUnauthorized')
+			req.claim = payload
+			next()
+		})
+	};
+	
+	static isSelf(req, res, next) {
+		if (parseInt(req.params.staffId) !== req.claim.id) throw new Error('userUnauthorized')
+		next()
+	};
+
+};
+
+
+module.exports = Auth;
