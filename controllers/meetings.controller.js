d449ad993270a41c7184280483e614d84ef5cc0a
Curtis Cawley <curtis.cawley@icloud.com>
2018-09-07 15:28:42 -0700

    implemented meetings controller.show method

--- controllers/meetings.controller.js
+++ controllers/meetings.controller.js
@@ -8,15 +8,23 @@ class MeetingsController extends Controller {
     super()
   };
 
+  static show(req, res, next) {
+    OrganizationsModel.show(req.params.id)
+      .then(() => SessionsModel.show(req.params.sid))
+      .then(() => MeetingsModel.show(req.params.mid))
+      .then(data => res.status(201).json({ data }))
+      .catch(err => next(err))
+  };
+
   static isValidMeetingCreate(req, res, next) {
     // validate.MeetingCreate(req.body)
-      
+
     //   .catch(err => next(err));
   };
 
   static isValidMeetingPatch(req, res, next) {
     // validate.meetingUpdate(req.body)
-      
+
     //   .catch(err => next(err));
   };
 
