3f8ef79295093658302b13ff1c368a2e88044a01
Justin <justhays124@gmail.com>
2018-09-08 11:49:04 -0700

    correcting old formatting issue

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,32 +5,32 @@ module.exports = name => {
     class Controller {
         
         static index(req, res, next) {
-            Model.index();
-                .then(data => res.status(201).json({ data }));
+            Model.index()
+                .then(data => res.status(201).json({ data }))
                 .catch(err => next(err));
         };
 
         static show(req, res, next) {
-            Model.show(req.params.id);
-                .then(data => res.status(201).json({ data }));
+            Model.show(req.params.id)
+                .then(data => res.status(201).json({ data }))
                 .catch(err => next(err));
         };
 
         static create(req, res, next) {
-            Model.create(req.body);
-                .then(data => res.status(201).json({ data }));
+            Model.create(req.body)
+                .then(data => res.status(201).json({ data }))
                 .catch(err => next(err));
         };
 
         static update(req, res, next) {
-            Model.update(req.params.id, req.body);
-                .then(data => res.status(200).json({ data }));
+            Model.update(req.params.id, req.body)
+                .then(data => res.status(200).json({ data }))
                 .catch(err => next(err));
         };
 
         static destroy(req, res, next) {
-            Model.destroy(req.params.id);
-                .then(data => res.status(202).json({ data }));
+            Model.destroy(req.params.id)
+                .then(data => res.status(202).json({ data }))
                 .catch(err => next(err));
         };
 
