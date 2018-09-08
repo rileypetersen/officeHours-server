fc61033807ac3781c908056e888b1f7ba54a03e4
Justin <justhays124@gmail.com>
2018-09-08 11:52:09 -0700

    correcting old formatting issue

--- controllers/organizations.controller.js
+++ controllers/organizations.controller.js
@@ -6,28 +6,28 @@ const bcrypt = require('bcryptjs');
 
 
 class OrganizationsController extends Controller {
-  constructor(){
-    super()
-  };
+	constructor(){
+		super()
+	};
 
-  static isValidOrgCreate(req, res, next) {
-    validate.organizationCreate(req.body)
-      .then(() =>  UsersModel.show(req.body.user_id))
-      .then(() => OrganizationsModel.getOrgByName(req.body.name))
-      .then(org => {
-        if (org !== undefined) throw new Error('alreadyOrg')
-        next()
-      })
-      .catch(err => next(err));
-  };
+	static isValidOrgCreate(req, res, next) {
+		validate.organizationCreate(req.body)
+			.then(() =>  UsersModel.show(req.body.user_id))
+			.then(() => OrganizationsModel.getOrgByName(req.body.name))
+			.then(org => {
+				if (org !== undefined) throw new Error('alreadyOrg')
+				next()
+			})
+			.catch(err => next(err));
+	};
 
-  static isValidOrgPatch(req, res, next) {
-    validate.orgUpdate(req.body)
-      .then(() => OrganizationsModel.show(req.params.id))
-      .then(() => OrganizationsModel.update(req.params.id, req.body))
-      .then(data => res.status(201).json({ data }))
-      .catch(err => next(err));
-  };
+	static isValidOrgPatch(req, res, next) {
+		validate.orgUpdate(req.body)
+			.then(() => OrganizationsModel.show(req.params.id))
+			.then(() => OrganizationsModel.update(req.params.id, req.body))
+			.then(data => res.status(201).json({ data }))
+			.catch(err => next(err));
+	};
 
 };
 
