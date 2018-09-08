1082fec9ada06262251859656840abb2a8aa3e9b
Justin <justhays124@gmail.com>
2018-09-08 14:27:06 -0700

    implemented auth for user data manipulation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,93 +4,33 @@ const jwt = require('jsonwebtoken');
 
 
 class Auth {
-
-	// Verify requestor is a currently valid account holder (user or admin)
-	static isAuthorized(req, res, next) {
-		// Validate and decode token
-		Token.verifyAndExtractHeaderToken(req.headers)
-			.catch(err => { throw new Error('invalidToken') })
-			// Check for and retrieve user from database
-			.then(token => UserModel.getUser(token.sub.id))
-			// Verify user
-			.then(user => {
-				if (!user) throw new Error('requestorInvalid')
-				next() // pass auth check
-			})
-			.catch(next) // fail auth check 
-	  }
-	
-	// Verify role is admin
-	static isAdmin(req, res, next) {
-		// Validate and decode token
-		Token.verifyAndExtractHeaderToken(req.headers)
-			.catch(err => { throw new Error('invalidToken') })
-			// Check for and retrieve user from database
-			.then(token => UserModel.getUser(token.sub.id))
-			// Verify user
-			.then(user => {
-				if (!user) throw new Error('requestorInvalid')
-				if (user.role !== 'admin') throw new Error('unauthorizedUser')
-				next() // pass auth check
-			})
-			.catch(next) // fail auth check
-	}
-	
-	// Verify user owns the USER TABLE resource...
+		
 	static isOwnerOfUser(req, res, next) {
-		const userId = req.params.id
-		// Validate and decode token
+		const userId = req.params.id;
 		Token.verifyAndExtractHeaderToken(req.headers)
 			.catch(err => { throw new Error('invalidToken') })
-			// Check for and retrieve user from database
-			.then(token => UserModel.getUser(token.sub.id))
-			// Verify User
+			.then(token => UsersModel.verifyUserId(token.sub.id))
 			.then(user => {
-				if (!user) throw new Error('requestorInvalid')
-				if (user.id != userId) throw new Error('unauthorizedUser')
-				next() // pass auth check
+				if (user.id != userId) throw new Error('unauthorizedUser');
+				next();
 			})
-			.catch(next) // fail auth check
-	}
+			.catch(err => next(err));
+	};
+	
+	// methods still needed:
+		// authorize owner of org for patch/delete
+		// authorise user type/status can create sessions
+		// authorise user type/status can create meetings
+		
+		// authorise user type/status can "view" sessions		
+		// authorise user type/status can "join" sessions
+		// authorise user type/status can "alter" sessions		
+		// authorise user type/status can "leave" sessions
 
-	// Verify admin or a user who owns the USER TABLE resource...
-	static isOwnerOfUserOrAdmin(req, res, next) {
-		const userId = req.params.id
-		// Validate and decode token
-		Token.verifyAndExtractHeaderToken(req.headers)
-			.catch(err => { throw new Error('invalidToken') })
-			// Check for and retrieve user from database
-			.then(token => UserModel.getUser(token.sub.id))
-			// Verify User
-			.then(user => {
-				if (!user) throw new Error('requestorInvalid')
-				if (!(user.role === 'admin' || (user.role === 'user' && user.id == userId))) throw new Error('unauthorizedUser')
-				next() // pass auth check
-			})
-			.catch(next) // fail auth check
-	}
-
-	// Verify user owns the REVIEW...
-	static isOwnerOfReview(req, res, next) {
-		const reviewId = req.params.id
-		// Validate and decode token
-		Token.verifyAndExtractHeaderToken(req.headers)
-			.catch(err => { throw new Error('invalidToken') })
-			// Check for and retrieve user and review from database
-			.then(token => {
-				const promises = [ UserModel.getUser(token.sub.id), ReviewModel.getOne(reviewId) ]
-				return Promise.all(promises)
-			})
-			// Verify User & that User owns Review
-			.then(results => {
-				const [ user, review ] = results
-				if (!user) throw new Error('requestorInvalid')
-				if (user.id != review.user_id) throw new Error('unauthorizedUser')
-				next() // pass auth check
-			})
-			.catch(next) // fail auth check
-	}
-
+		// authorise user type/status can "view" meetings				
+		// authorise user type/status can "join" meetings
+		// authorise user type/status can "alter" meetings		
+		// authorise user type/status can "leave" meetings	
 };
 
 
