590b97a2d2f69bc56a0a8a9fc5a10864e94bed57
Justin <justhays124@gmail.com>
2018-09-08 12:09:06 -0700

    corrected indentation formatting issue

--- knexfile.js
+++ knexfile.js
@@ -1,38 +1,39 @@
-const path = require('path')
-require('dotenv').load()
+const path = require('path');
+require('dotenv').load();
 
 module.exports = {
 
-  development: {
-    client: 'pg',
-    connection: `postgres://localhost/${process.env.DATABASE_NAME}`,
-    migrations: {
-      directory: path.join(__dirname, 'db', 'migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, 'db', 'seeds')
-    }
-  },
+	development: {
+		client: 'pg',
+		connection: `postgres://localhost/${process.env.DATABASE_NAME}`,
+		migrations: {
+			directory: path.join(__dirname, 'db', 'migrations')
+		},
+		seeds: {
+			directory: path.join(__dirname, 'db', 'seeds')
+		}
+	},
 
-  test: {
-    client: 'pg',
-    connection: `postgres://localhost/${process.env.DATABASE_NAME}`,
-    migrations: {
-      directory: path.join(__dirname, 'db', 'migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, 'db', 'seeds')
-    }
-  },
+	test: {
+		client: 'pg',
+		connection: `postgres://localhost/${process.env.DATABASE_NAME}`,
+		migrations: {
+			directory: path.join(__dirname, 'db', 'migrations')
+		},
+		seeds: {
+			directory: path.join(__dirname, 'db', 'seeds')
+		}
+	},
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: path.join(__dirname, 'db', 'migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, 'db', 'seeds')
-    }
-  }
-}
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: path.join(__dirname, 'db', 'migrations')
+		},
+		seeds: {
+			directory: path.join(__dirname, 'db', 'seeds')
+		}
+	}
+	
+};
