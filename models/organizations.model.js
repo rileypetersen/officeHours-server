723d1ad352008a2f7f3b09e94db479d18511b3c5
Justin <justhays124@gmail.com>
2018-09-08 12:02:20 -0700

    correcting old formatting issue

--- models/organizations.model.js
+++ models/organizations.model.js
@@ -1,20 +1,19 @@
 const Model = require('./model.js')(`organizations`);
 const knex = require('../db/knex');
-const bcrypt = require('bcryptjs')
 
 
 class OrganizationsModel extends Model {
-  constructor(){
-    super()
-  };
+	constructor(){
+		super()
+	};
 
-  static getOrgByName(name) {
-    return knex('organizations')
-      .where({ name })
-      .first()
-  };
+	static getOrgByName(name) {
+		return knex('organizations')
+			.where({ name })
+			.first()
+	};
 
 };
 
 
-module.exports = OrganizationsModel
+module.exports = OrganizationsModel;
