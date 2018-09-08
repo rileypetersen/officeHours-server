c498a64cfd2123d67e35710594c25992bcfb20bb
Justin <justhays124@gmail.com>
2018-09-08 12:07:36 -0700

    correcting old formatting issue

--- app.js
+++ app.js
@@ -20,15 +20,15 @@ app.use('/api/organizations/:id/sessions/:sid/meetings', MeetingsRouter);
 app.use('/api/organizations/:id/tags', TagsRouter);
 
 app.use((req, res) => {
-  const status = 404;
-  const message = `Could not ${req.method} ${req.path}`;
-  res.status(status).json({ status, message });
+	const status = 404;
+	const message = `Could not ${req.method} ${req.path}`;
+	res.status(status).json({ status, message });
 });
 
 app.use((err, req, res, next) => {
-  err = processErrorMessage(err);
-  if (process.env.NODE_ENV !== 'test') console.error(err)
-  res.status(err.status).json(err);
+	err = processErrorMessage(err);
+	if (process.env.NODE_ENV !== 'test') console.error(err);
+	res.status(err.status).json(err);
 });
 
 app.listen(port, () => console.log(`On port: ${port}`));
