58fc80287bc63f3fd4bb50f8ddb41a799b368856
Justin <justhays124@gmail.com>
2018-09-08 11:43:36 -0700

    correcting old formatting issue

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,39 +1,41 @@
 module.exports = name => {
-    const Model = require(`../models/${ name }.model.js`)
+
+    const Model = require(`../models/${ name }.model.js`);
     
     class Controller {
         
         static index(req, res, next) {
-            Model.index()
-                .then(data => res.status(201).json({ data }))
-                .catch(err => next(err))
+            Model.index();
+                .then(data => res.status(201).json({ data }));
+                .catch(err => next(err));
         };
 
         static show(req, res, next) {
-            Model.show(req.params.id)
-                .then(data => res.status(201).json({ data }))
-                .catch(err => next(err))
+            Model.show(req.params.id);
+                .then(data => res.status(201).json({ data }));
+                .catch(err => next(err));
         };
 
         static create(req, res, next) {
-            Model.create(req.body)
-                .then(data => res.status(201).json({ data }))
-                .catch(err => next(err))
+            Model.create(req.body);
+                .then(data => res.status(201).json({ data }));
+                .catch(err => next(err));
         };
 
         static update(req, res, next) {
-            Model.update(req.params.id, req.body)
-                .then(data => res.status(200).json({ data }))
-                .catch(err => next(err))
+            Model.update(req.params.id, req.body);
+                .then(data => res.status(200).json({ data }));
+                .catch(err => next(err));
         };
 
         static destroy(req, res, next) {
-            Model.destroy(req.params.id)
-                .then(data => res.status(202).json({ data }))
-                .catch(err => next(err))
+            Model.destroy(req.params.id);
+                .then(data => res.status(202).json({ data }));
+                .catch(err => next(err));
         };
 
-    }
+    };
 
-    return Controller
+    return Controller;
+
 };
