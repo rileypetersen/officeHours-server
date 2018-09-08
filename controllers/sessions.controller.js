453e47431f30f3e281ebe5ae03b4dc8699b66bfd
Justin <justhays124@gmail.com>
2018-09-08 11:53:28 -0700

    correcting old formatting issue

--- controllers/sessions.controller.js
+++ controllers/sessions.controller.js
@@ -2,9 +2,9 @@ const Controller = require('./Controller.js')(`Sessions`);
 const { UsersModel, OrganizationsModel, SessionsModel } = require('../models');
 const validate = require('../middleware/validations');
 const Token = require('../middleware/token');
-const moment = require('moment')
-const dateFormat = 'MM-DD-YYYY'
-const timeFormat = '+-HH:mm a'
+const moment = require('moment');
+const dateFormat = 'MM-DD-YYYY';
+const timeFormat = '+-HH:mm a';
 
 
 class SessionsController extends Controller {
@@ -13,10 +13,10 @@ class SessionsController extends Controller {
     };
 
     static index(req, res, next) {
-      OrganizationsModel.show(req.params.id)
-        .then(() => SessionsModel.index(req.params.id))
-        .then(data => res.status(201).json({ data }))
-        .catch(err => next(err));
+        OrganizationsModel.show(req.params.id)
+            .then(() => SessionsModel.index(req.params.id))
+            .then(data => res.status(201).json({ data }))
+            .catch(err => next(err));
     };
 
     static show(req, res, next) {
@@ -28,14 +28,14 @@ class SessionsController extends Controller {
 
     static isValidSessionCreate(req, res, next) {
         // !!!!!!!!! temp use of moment on: DATE && START_TIME for route testing !!!!!!!!!
-        req.body.date = moment(req.body.date, dateFormat).format()
-        req.body.start_time = moment(req.body.start_time, timeFormat).format()
+        req.body.date = moment(req.body.date, dateFormat).format();
+        req.body.start_time = moment(req.body.start_time, timeFormat).format();
 
         validate.sessionCreate(req.body, parseInt(req.params.id))
             .then(() => OrganizationsModel.show(req.params.id))
             .then(() =>  UsersModel.show(req.body.user_id))
             .then(user => {
-                if (user.can_create_session !== true) throw new Error('userCanNotCreateSession')
+                if (user.can_create_session !== true) throw new Error('userCanNotCreateSession');
             })
             .then(() => next())
             .catch(err => next(err));
