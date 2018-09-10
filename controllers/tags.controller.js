494745cbd1317c7f83e47f70e1b97ff471b0b039
Justin <justhays124@gmail.com>
2018-09-10 14:27:35 -0700

    show and add/remove tags for user working

--- controllers/tags.controller.js
+++ controllers/tags.controller.js
@@ -19,10 +19,18 @@ class TagsController extends Controller {
 		
 		//   .catch(err => next(err));
 	};
+	
+	static showUserTags(req, res, next) {
+		UsersModel.show(req.params.id)
+			.then(() => TagsModel.showUserTags(req.params.id))
+			.then(data => res.status(200).json({ data }))
+			.catch(err => next(err));
+	};
 
 	static addOrRemoveTag(req, res, next) {
-		TagsModel.addOrRemoveTag(req.body)
-			.then(() => TagsModel.getUserTags(req.body.user_id))
+		UsersModel.show(req.params.id)		
+			.then(() => TagsModel.addOrRemoveTag(req.params.id, req.body.tag_id))
+			.then(() => TagsModel.showUserTags(req.params.id))
 			.then(data => res.status(200).json({ data }))
 		  	.catch(err => next(err));
 	};
