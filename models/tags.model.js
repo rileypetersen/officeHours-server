a9771a27415a5d2ce53dc16467b669786fca5bc3
Justin <justhays124@gmail.com>
2018-09-10 14:00:53 -0700

    routing for user's tags

--- models/tags.model.js
+++ models/tags.model.js
@@ -7,6 +7,27 @@ class TagsModel extends Model {
 		super()
 	};
 
+	static addOrRemoveTag({ user_id, tag_id }) {
+		return knex('users_tags')
+			.where({ user_id, tag_id })
+			.first()
+			.then(result => {
+				if (!result) {
+				return knex('users_tags')
+					.insert({ user_id, tag_id })
+				} else {
+				return knex('users_tags')
+					.where({ user_id, tag_id })
+					.del()
+				}
+			})
+	};
+
+	static getUserTags(user_id) {
+		return knex('users_tags')
+			.where({ user_id })
+	};
+
 };
 
 
