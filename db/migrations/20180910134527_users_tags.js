ce49c3e2feeec70f2e0f3f649a4f1128d5a3acb0
Justin <justhays124@gmail.com>
2018-09-10 14:00:22 -0700

    users_tags migrations creation

---
+++ db/migrations/20180910134527_users_tags.js
@@ -0,0 +1,16 @@
+// 'users_tags' table
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users_tags', table => {
+      table.increments();
+      table.integer('user_id').notNullable();
+      table.foreign('user_id').references('users.id').onDelete('CASCADE');
+      table.integer('tag_id').notNullable();
+      table.foreign('tag_id').references('tags.id').onDelete('CASCADE');
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('users_tags');
+  };
+  
