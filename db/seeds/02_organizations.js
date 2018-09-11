19cf07a51b723fd6d13fe64628a13268e42ad494
Justin <justhays124@gmail.com>
2018-09-10 22:45:42 -0700

    updation of organizer_id column

--- db/seeds/02_organizations.js
+++ db/seeds/02_organizations.js
@@ -4,7 +4,7 @@ exports.seed = (knex, Promise) => {
       return knex('organizations').insert([
         {
           id: 1,
-          user_id: 1,
+          organizer_id: 1,
           name: 'WTIA',
           short_description: 'Washington Technology Industry Association is a trade association unifying the voice of the technology community in Washington state.',
           long_description: 'WTIA is an influential co-op of 800 tech companies large and small. We are pragmatic partners with education and government leaders building a better world.',
@@ -14,7 +14,7 @@ exports.seed = (knex, Promise) => {
         },
         {
           id: 2,
-          user_id: 2,
+          organizer_id: 2,
           name: 'Alliance of Angels',
           short_description: 'Largest and most active group of angel investors in the Pacific Northwest.',
           long_description: 'We are the largest and most active angel group in the Pacific Northwest. Each year, we invest $10M+ into 20+ companies. Since 1997, we have invested $100M+ into 200+ companies and celebrated 40+ exits generating more than a billion dollars of returns. Our network of 140+ active angel investors is further augmented by a $6.6M seed fund.',
