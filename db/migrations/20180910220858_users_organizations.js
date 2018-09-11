8aadc8a3aa67bc799bbc4a032080cd15c24cc3d8
Justin <justhays124@gmail.com>
2018-09-10 22:21:57 -0700

    transfer user_org related info

--- db/migrations/20180910220858_users_organizations.js
+++ db/migrations/20180910220858_users_organizations.js
@@ -1,10 +1,13 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('users_organizations', table => {
-      table.increments();
-      table.integer('user_id').notNullable();
-      table.foreign('user_id').references('users.id').onDelete('CASCADE');
-      table.integer('organization_id').notNullable();
-      table.foreign('organization_id').references('organizations.id').onDelete('CASCADE');
+        table.increments();
+        table.integer('user_id').notNullable();
+        table.foreign('user_id').references('users.id').onDelete('CASCADE');
+        table.integer('organization_id').notNullable();
+        table.foreign('organization_id').references('organizations.id').onDelete('CASCADE');
+        table.string('user_type').defaultTo('member');
+        table.string('title').notNullable();
+        table.boolean('can_create_session').notNullable().defaultTo(false);        
     });
   };
   
