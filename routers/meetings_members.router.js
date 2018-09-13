21aee8e30d139a98aeff97f54f5cda8747ce87b4
Justin <justhays124@gmail.com>
2018-09-12 17:57:53 -0700

    formatting

--- routers/meetings_members.router.js
+++ routers/meetings_members.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
+const { MeetingsController } = require('../controllers');
 
 
 // router.get('/', MeetingsController.getMember)
